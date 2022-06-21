31d0ffe374f542ec297645a03ae9870d51e16db8
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-06-22 00:56:08 +0100

    Added Viewbinding

--- app/src/main/java/com/example/kalqulatorapp/MainActivity.kt
+++ app/src/main/java/com/example/kalqulatorapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.kalqulatorapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kalqulatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
