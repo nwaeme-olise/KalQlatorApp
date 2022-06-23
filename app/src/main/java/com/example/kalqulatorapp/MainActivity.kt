82f9a78f2e15f25924bc833cf51299ecb6e862f7
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-06-23 17:24:16 +0100

    Added a function and Click Listeners for displaying button text within app

--- app/src/main/java/com/example/kalqulatorapp/MainActivity.kt
+++ app/src/main/java/com/example/kalqulatorapp/MainActivity.kt
@@ -3,17 +3,61 @@ package com.example.kalqulatorapp
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.widget.EditText
 import com.example.kalqulatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var  display: EditText
+    private lateinit var stringDisplay: SpannableStringBuilder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.expressionEditText.showSoftInputOnFocus = false
+        display = binding.expressionEditText
+        display.showSoftInputOnFocus = false
+        display.setText("")
+        stringDisplay = SpannableStringBuilder(display.text.toString())
+
+        binding.buttonOne.setOnClickListener{updateDisplayOnButtonClick("1")}
+
+        binding.buttonTwo.setOnClickListener { updateDisplayOnButtonClick("2")}
+
+        binding.buttonThree.setOnClickListener { updateDisplayOnButtonClick("3") }
+
+        binding.buttonFour.setOnClickListener { updateDisplayOnButtonClick("4") }
+        
+        binding.buttonFive.setOnClickListener { updateDisplayOnButtonClick("5") }
+        
+        binding.buttonSix.setOnClickListener { updateDisplayOnButtonClick("6") }
+        
+        binding.buttonSeven.setOnClickListener { updateDisplayOnButtonClick("7") }
+        
+        binding.buttonEight.setOnClickListener { updateDisplayOnButtonClick("8") }
+        
+        binding.buttonNine.setOnClickListener { updateDisplayOnButtonClick("9") }
+        
+        binding.buttonDot.setOnClickListener { updateDisplayOnButtonClick(".") }
+        
+        binding.buttonAdd.setOnClickListener { updateDisplayOnButtonClick("+") }
+        
+        binding.buttonSubtract.setOnClickListener { updateDisplayOnButtonClick("-") }
+        
+        binding.buttonMultiply.setOnClickListener { updateDisplayOnButtonClick(getString(R.string.times)) }
+
+        binding.buttonDivide.setOnClickListener { updateDisplayOnButtonClick(getString(R.string.divide)) }
+
+
 
     }
+
+
+    private fun updateDisplayOnButtonClick(buttonStringValue: String){
+        stringDisplay.append(buttonStringValue)
+        display.text = stringDisplay
+    }
 }
+
